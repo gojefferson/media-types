3a16f6ed07f23f66e17b1ac309c3a8448b3f36e1
gojefferson <jeff@lawprct.com>
2017-06-26 13:55:28 -0400

    ignore IANA - Tika only

--- parse-types.py
+++ parse-types.py
@@ -1,62 +1,62 @@
-import csv
 import json
-import os
 import xml.etree.ElementTree as ET
 
-files = [
-    'application.csv',
-    'audio.csv',
-    'font.csv',
-    'image.csv',
-    'message.csv',
-    'model.csv',
-    'multipart.csv',
-    'text.csv',
-    'video.csv'
-]
+MS_WORD = 'Word Document'
+EMAIL = 'Email'
+ZIPFILE = 'Archive'
+CSV = 'CSV'
+POWERPOINT = 'PowerPoint'
+EMAIL_CONTAINER = 'Email Archive'
+SPREADSHEET = 'Excel'
 
+READABLE_MAPPINGS = {
 
-mappings = {
-    'application/msword': 'Word',
-    'application/vnd.ms-word.template.macroEnabled.12': 'Word',
-    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': 'Word',
+    # Word formats
+    'application/msword': MS_WORD,
+    'application/vnd.ms-word.template.macroEnabled.12': MS_WORD,
+    'application/vnd.openxmlformats-officedocument.wordprocessingml.document': MS_WORD,
+    'application/msword2': MS_WORD,
+    'application/vnd.ms-word.document.macroenabled.12': MS_WORD,
+    'application/vnd.ms-word.template.macroenabled.12': MS_WORD,
+
+    # Email containers
+    'application/vnd.ms-outlook-pst': EMAIL_CONTAINER,
+
+    # Emails
+    'message/x-emlx': EMAIL,
+    'application/vnd.ms-outlook': EMAIL,
+
+    # Archives
+    'application/gzip': ZIPFILE,
+
+    # CSV
+    'text/csv': CSV,
+
+    # PowerPoint
+    'application/vnd.ms-powerpoint': POWERPOINT,
+    'application/vnd.ms-powerpoint.presentation.macroenabled.12': POWERPOINT,
+
+    # Excel
+    'application/vnd.ms-excel': SPREADSHEET,
+
 }
-
 
 if __name__ == '__main__':
     types = {}
-    for file in files:
-        fpath = os.path.join(os.getcwd(), 'iana', file)
-        with open(fpath) as _file:
-            reader = csv.DictReader(_file)
-            mime_types = [r for r in reader]
-            group = file.split('.')[0].capitalize()
-            for type_obj in mime_types:
-                type_obj['group'] = group
-                del type_obj['Name']
-                del type_obj['Reference']
-                key = type_obj['Template'].lower()
-                del type_obj['Template']
-                type_obj['IANA'] = True
-                types[key] = type_obj
 
     tree = ET.parse('tika/tika-types.xml')
     root = tree.getroot()
 
     for node in root.getchildren():
-        print(f'type: {node.get("type")}')
-        print(f'in types?: {node.get("type") in types.keys()}')
         globs = [g.get('pattern') for g in node.getchildren() if g.tag == 'glob']
         mime_type = node.get('type').lower()
-        if mime_type in types.keys():
-            types['extensions'] = globs
-            types['Tika'] = True
-        else:
-            types[mime_type] = {
-                'extensions': globs,
-                'Tika': True,
-                'group': mime_type.split('/')[0].capitalize()
-            }
+        group = mime_type.split('/')[0].capitalize()
+        types[mime_type] = {
+            'Extensions': globs,
+            'Tika': True,
+            'Human Readable Name': READABLE_MAPPINGS.get(mime_type, group),
+            'Group': group
+        }
 
     with open('types.json', 'wt') as write_file:
         write_file.write(json.dumps(types, sort_keys=True, indent=4))
