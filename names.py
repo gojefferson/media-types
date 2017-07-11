e19cea6ed6ad16873e32f2115deaf79c16340297
gojefferson <jeff@lawprct.com>
2017-07-11 16:20:28 -0400

    use modules

---
+++ names.py
@@ -0,0 +1,10 @@
+
+
+class FileName:
+    MS_WORD = 'Word Document'
+    EMAIL = 'Email'
+    ZIPFILE = 'Archive'
+    CSV = 'CSV'
+    POWERPOINT = 'PowerPoint'
+    EMAIL_CONTAINER = 'Email Archive'
+    SPREADSHEET = 'Excel'
